f3e21772cc7866f27427cf266a252c22a5100388
Moisés Jorge <moisesjorge30@gmail.com>
2024-05-24 16:03:07 +0100

    created the first model: Pessoa

---
+++ src/main/java/com/projecto/api/model/Pessoa.java
@@ -0,0 +1,20 @@
+package com.projecto.api.model;
+
+public class Pessoa {
+    
+    //START: Atiributts
+        private String nome;
+        private int idade;
+    //END: Atiributts
+
+    //Start: Getter e Setter for 'nome'
+        public String getNome() { return nome; }
+        public void setNome(String nome) { this.nome = nome; }
+    //END: Getter e Setter for 'nome'
+
+    //Start: Getter e Setter for 'idade'
+        public int getIdade() { return idade; }
+        public void setIdade(int idade) { this.idade = idade; }
+    //END: Getter e Setter for 'idade'
+
+}
