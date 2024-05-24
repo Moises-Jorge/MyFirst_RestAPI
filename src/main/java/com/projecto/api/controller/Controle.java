6c7b5dfe26f1d42fc72619c112bb6f76e1d3932f
Moisés Jorge <moisesjorge30@gmail.com>
2024-05-24 15:33:10 +0100

    created the second route with PathVariable

--- src/main/java/com/projecto/api/controller/Controle.java
+++ src/main/java/com/projecto/api/controller/Controle.java
@@ -1,13 +1,21 @@
 package com.projecto.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controle {
     
+    // First Route
     @GetMapping("")
     public String mensagem() {
         return "Hello World";
+    }
+
+    // Second Route
+    @GetMapping("/boasVindas/{nome}")
+    public String boasVindas(@PathVariable String nome) {
+        return "Seja bem vindo(a)" + nome;
     }
 }
