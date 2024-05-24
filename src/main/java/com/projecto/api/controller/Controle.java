a690c3730d0de5f7bf09ebdf41ee32615c5d5ae2
Moisés Jorge <moisesjorge30@gmail.com>
2024-05-24 16:18:22 +0100

    created third route: /pessoa with annotation RequestBody & PostMapping - 8th class with Ralf Lima

--- src/main/java/com/projecto/api/controller/Controle.java
+++ src/main/java/com/projecto/api/controller/Controle.java
@@ -2,7 +2,11 @@ package com.projecto.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.projecto.api.model.Pessoa;
 
 @RestController
 public class Controle {
@@ -17,5 +21,11 @@ public class Controle {
     @GetMapping("/boasVindas/{nome}")
     public String boasVindas(@PathVariable String nome) {
         return "Seja bem vindo(a)" + nome;
+    }
+
+    // Third Route: Route Pessoa
+    @PostMapping("/pessoa")
+    public Pessoa pessoa(@RequestBody Pessoa pessoaP) {
+        return pessoaP;
     }
 }
