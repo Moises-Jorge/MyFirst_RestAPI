9ce20b8de9286bd54820c1ff2af810e4987e713d
Moisés Jorge <moisesjorge30@gmail.com>
2024-05-28 05:53:59 +0100

    configurated the model Pessoa.java to generated a table

--- src/main/java/com/projecto/api/model/Pessoa.java
+++ src/main/java/com/projecto/api/model/Pessoa.java
@@ -1,11 +1,31 @@
 package com.projecto.api.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "pessoas")
 public class Pessoa {
     
     //START: Atiributts
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private int codigo;
         private String nome;
         private int idade;
     //END: Atiributts
+
+    //Start: Getter e Setter for 'codigo'
+        public int getCodigo() {
+            return codigo;
+        }
+        public void setCodigo(int codigo) {
+            this.codigo = codigo;
+        }
+    //END: Getter e Setter for 'codigo'
 
     //Start: Getter e Setter for 'nome'
         public String getNome() { return nome; }
